a01e80f535fb03292647a6e43dbb3793933576f9
Gustavo <126250938+LuisGustavo16@users.noreply.github.com>
2024-11-01 16:00:22 -0300

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
             ]);
             $token = $user->createToken('token-name')->plainTextToken;
             return $this->success([
-                'access_token' => $token,
+                'token' => $token,
                 'user' => $user,
             ], "Cadastro realizado com sucesso!!!");
         } catch (\Throwable $th) {
@@ -48,7 +48,7 @@ class AuthController extends Controller
             $user = User::where('email', $request['email'])->firstOrFail();
             $token = $user->createToken('auth_token')->plainTextToken;
             return $this->success([
-                'access_token' => $token,
+                'token' => $token,
                 'user' => $user
             ], "Login realizado!!!");
         } catch (\Throwable $th) {
