5d2b06bb61ea356fd8c761271d2c6c1c5f5537f5
Gustavo <126250938+LuisGustavo16@users.noreply.github.com>
2024-11-07 10:16:45 -0300

    Update ReservasController.php

--- app/Http/Controllers/API/ReservasController.php
+++ app/Http/Controllers/API/ReservasController.php
@@ -17,7 +17,7 @@ class ReservasController extends Controller
         try {
         $dados = Reserva::create([
             'idAluno' => $request['idAluno'],
-            'dia' => $request->[dia'],
+            'dia' => $request[dia'],
             'horarioInicio' => $request['horarioInicio'],
             'horarioFim' => $request['horarioFim'],
             'finalidade' => $request['finalidade'],
@@ -25,8 +25,7 @@ class ReservasController extends Controller
             'tipo' => $request['tipo'],
             'numeroPessoas' => $request['numeroPessoas'],
         ]);
-
-        return $this->success([], "Cadastro realizado com sucesso!!!");
+            return $this->success([], "Cadastro realizado com sucesso!!!");
         } catch (\Throwable $th) {
                 return $this->error("Erro ao registrar a presença!!!", 401, $th->getMessage());
             }
