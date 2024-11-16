016008b02e59f5f1c0bdf184ea3c8b302346fa7f
Gustavo <126250938+LuisGustavo16@users.noreply.github.com>
2024-11-16 12:34:26 -0300

    Update ReservasController.php

--- app/Http/Controllers/API/ReservasController.php
+++ app/Http/Controllers/API/ReservasController.php
@@ -9,34 +9,34 @@ use Iluminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Reserva;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
-
 class ReservasController extends Controller
 {
     use ApiResponse;
     use HasApiTokens;
-    public function store(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'idAluno' => 'required|integer',
-        'dia' => 'required|date',
-        'horarioInicio' => 'required',
-        'horarioFim' => 'required',
-        'finalidade' => 'required|string',
-        'status' => 'required|string',
-        'tipo' => 'required|string',
-        'numeroPessoas' => 'required|integer',
-        'local' => 'required|string',
-    ]);
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'idAluno' => 'required|integer',
+            'dia' => 'required|date',
+            'horarioInicio' => 'required',
+            'horarioFim' => 'required',
+            'finalidade' => 'required|string',
+            'status' => 'required|string',
+            'tipo' => 'required|string',
+            'numeroPessoas' => 'required|integer',
+            'local' => 'required|string',
+        ]);
 
-    if ($validator->fails()) {
-        return $this->error("Erro de validação!", 400, $validator->errors());
-    }
+        if ($validator->fails()) {
+            return $this->error("Erro de validação!", 400, $validator->errors());
+        }
 
-    try {
-        $dados = Reserva::create($request->all());
-        return $this->success([], "Cadastro realizado com sucesso!!!");
-    } catch (\Throwable $th) {
-        return $this->error("Erro ao registrar a presença!!!", 401, $th->getMessage());
-    }
+        try {
+            $dados = Reserva::create($request->all());
+            return $this->success([], "Cadastro realizado com sucesso!!!");
+        } catch (\Throwable $th) {
+            return $this->error("Erro ao registrar a presença!!!", 401, $th->getMessage());
+        }
     }
 
     public function index()
@@ -54,4 +54,6 @@ class ReservasController extends Controller
             'dados' => $dados,
         ]);
     }
+
+
 }
