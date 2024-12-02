263c9fdc178c9ba3fd00ac2072e034c77f4b85ec
LuisGustavo16 <guluisgustavo300@gmail.com>
2024-12-02 14:11:09 -0300

    texto da sua escolha

--- resources/views/Alunos/telaEspera.blade.php
+++ resources/views/Alunos/telaEspera.blade.php
@@ -8,7 +8,16 @@
 
 </head>
 <body>
-    <h1 class="sigee">SIGEE</h1>
-    <h1 class="msg">Seu cadastro foi realizado com sucesso! Espere a professora Gabriela avaliar seu registro para que você possa acessar o aplicativo...</h1>
+<div class="modal">
+        <div class="modal-content">
+            <h1 class="msg">Seu cadastro foi realizado com sucesso! <br> Espere a professora Gabriela avaliar seu registro para que você possa acessar o aplicativo...</h1>
+            <hr>
+            <div class="contact-info">
+                <h3>Informações de Contato</h3>
+                <p><span class="label">Email:</span> <span class="info">britotpleticia@gmail.com</span></p>
+                <p><span class="label">Telefone:</span> <span class="info">(35) 98828-9379</span></p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
